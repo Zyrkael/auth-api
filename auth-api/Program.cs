0d62f8b8a147210c22689c0d778194cbbc3e9eb0
Zyrkael <sonmatrix.dev@gmail.com>
2026-04-28 16:57:48 +0700

    Add Scalar.AspNetCore integration and update launch settings

--- auth-api/Program.cs
+++ auth-api/Program.cs
@@ -1,5 +1,7 @@
 using auth_api.Extensions;
 
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -16,6 +18,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("/docs", options =>
+    {
+        options.Title = "Auth Docs 🚀";
+
+        options.Layout = ScalarLayout.Modern;
+
+        options.Theme = ScalarTheme.DeepSpace;
+    });
 }
 
 
