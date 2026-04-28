7170565322eb97f1ea08996c8b9dc3ee47802d7b
Zyrkael <sonmatrix.dev@gmail.com>
2026-04-28 21:47:42 +0700

    style: rename constants to UPPER_SNAKE_CASE in AppConstants

--- auth-api/Configurations/AppConstants.cs
+++ auth-api/Configurations/AppConstants.cs
@@ -4,15 +4,15 @@ public static class AppConstants
 {
     public static class ConnectionStrings
     {
-        public const string SectionName = "ConnectionStrings";
-        public const string SqlServerConnection = "SqlServerConnection";
-        public const string MySqlConnection = "MySqlConnection";
-        public const string DbProvider = "DbProvider";
+        public const string SECTION_NAME = "ConnectionStrings";
+        public const string SQL_SERVER_CONNECTION = "SqlServerConnection";
+        public const string MY_SQL_CONNECTION = "MySqlConnection";
+        public const string DB_PROVIDER = "DbProvider";
     }
 
     public static class DbProviders
     {
-        public const string SqlServer = "SqlServer";
-        public const string MySql = "MySql";
+        public const string SQL_SERVER = "SqlServer";
+        public const string MY_SQL = "MySql";
     }
 }
--- auth-api/Extensions/DatabaseExtensions.cs
+++ auth-api/Extensions/DatabaseExtensions.cs
@@ -8,18 +8,18 @@ public static class DatabaseExtensions
 {
     public static void AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
     {
-        var dbProvider = configuration.GetSection(AppConstants.ConnectionStrings.SectionName)[AppConstants.ConnectionStrings.DbProvider];
+        var dbProvider = configuration.GetSection(AppConstants.ConnectionStrings.SECTION_NAME)[AppConstants.ConnectionStrings.DB_PROVIDER];
 
         services.AddDbContext<AppDbContext>(options =>
         {
-            if (dbProvider?.Equals(AppConstants.DbProviders.MySql, StringComparison.OrdinalIgnoreCase) == true)
+            if (dbProvider?.Equals(AppConstants.DbProviders.MY_SQL, StringComparison.OrdinalIgnoreCase) == true)
             {
-                var connectionString = configuration.GetConnectionString(AppConstants.ConnectionStrings.MySqlConnection);
+                var connectionString = configuration.GetConnectionString(AppConstants.ConnectionStrings.MY_SQL_CONNECTION);
                 options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
             }
             else
             {
-                var connectionString = configuration.GetConnectionString(AppConstants.ConnectionStrings.SqlServerConnection);
+                var connectionString = configuration.GetConnectionString(AppConstants.ConnectionStrings.SQL_SERVER_CONNECTION);
                 options.UseSqlServer(connectionString);
             }
         });
