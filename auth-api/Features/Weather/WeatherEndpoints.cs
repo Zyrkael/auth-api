dc9555782f7f3dfc7ef4f7c9f5889ba946ce35bd
Zyrkael <sonmatrix.dev@gmail.com>
2026-04-28 14:56:21 +0700

    Add WeatherForecast and WeatherEndpoints for weather data retrieval

---
+++ auth-api/Features/Weather/WeatherEndpoints.cs
@@ -0,0 +1,28 @@
+﻿namespace auth_api.Features.Weather;
+
+public static class WeatherEndpoints
+{
+    public static IEndpointRouteBuilder MapWeatherEndpoints(
+        this IEndpointRouteBuilder app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Hot", "Warm"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5)
+                .Select(index => new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ));
+
+            return forecast;
+        });
+
+        return app;
+    }
+}
+
--- auth-api/Program.cs
+++ auth-api/Program.cs
@@ -1,39 +1,24 @@
+using auth_api.Contexts;
+using auth_api.Features.Weather;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IAppDbContext>(sp =>
+    sp.GetRequiredService<AppDbContext>());
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
-app.MapGet("ping", () =>
-{
-    return "pong";
-});
+app.MapWeatherEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
