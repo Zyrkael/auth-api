37865733381b68a6a994f88c2ecc890789f754ba
Zyrkael <sonmatrix.dev@gmail.com>
2026-04-28 21:27:46 +0700

    feat: add DbProvider support for SQL Server and MySQL

--- auth-api/Extensions/DatabaseExtensions.cs
+++ auth-api/Extensions/DatabaseExtensions.cs
@@ -7,8 +7,20 @@ public static class DatabaseExtensions
 {
     public static void AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var dbProvider = configuration.GetSection("ConnectionStrings")["DbProvider"];
+
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        {
+            if (dbProvider?.Equals("MySql", StringComparison.OrdinalIgnoreCase) == true)
+            {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+            else
+            {
+                options.UseSqlServer(connectionString);
+            }
+        });
         
         services.AddScoped<IAppDbContext>(sp => sp.GetRequiredService<AppDbContext>());
     }
